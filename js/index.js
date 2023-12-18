c024f328b85d1e1f4d019b5b312673b07b4c2266
Volodymyr Dziuba <vovadziuba93@gmail.com>
2023-12-18 15:42:41 +0200

    added function for checking empty value

--- js/index.js
+++ js/index.js
@@ -3,13 +3,17 @@ const userInput = prompt("Введіть числа, через кому:");
 if(userInput === null || userInput.trim() === ""){
     alert("Ви нічого не ввели. Бувайте!");
 } else {
-    let numberLoop = userInput.split(",").map(Number);
+    const numberLoop = userInput.split(",").map(value => value.trim() === "" ? NaN : Number(value));// Пояснення на 18 рядку.
     if(numberLoop.some(isNaN)){
         alert("Одне чи декілька введених даних є не коректними, або не є числом. Не забувайте про кому!");
     } else {
-        let numberLoopSum = numberLoop.reduce((sum, current) => sum + current, 0);
-        let loopLenght = numberLoop.length;
+        const numberLoopSum = numberLoop.reduce((sum, current) => sum + current, 0);
+        const loopLenght = numberLoop.length;
         alert(`Сума введених Вами чисел: ${numberLoopSum}, ви ввели ${loopLenght} символів.`);
         
     }
 }
+
+
+//.map(value => value.trim() === "" ? NaN : Number(value)); - До кожного елементу масиву застосовується функція, яка перевіряє, чи рядок є прожній value.trim() === "".
+//Якщо рядок порожній, то елемент замінюється на NaN.
